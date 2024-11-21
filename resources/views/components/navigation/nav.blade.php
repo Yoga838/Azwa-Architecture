275323256a15f5f8fec7f6468e8655dde4926de6
shrmanto <shrmantoch@gmail.com>
2024-11-21 21:03:33 +0700

    feat: component navbar

--- resources/views/components/navigation/nav.blade.php
+++ resources/views/components/navigation/nav.blade.php
@@ -1,34 +1,34 @@
-<nav class="fixed top-0 z-50">
-    <div class="flex items-center bg-white py-6 px-6">
-        <div class="logo">
-            <div class="">
-                <Image src="{{ asset('assets/img/LogoAzwa.png') }}" alt="Brand" class="" />
+<nav class="w-full fixed top-0 z-50">
+    <div class="flex items-center justify-between bg-white py-6 px-[40px]">
+        <div class="cursor-pointer">
+            <div class="w-auto h-auto bg-[#515151] px-[28px] py-[8px] rounded-lg">
+                <Image src="{{ asset('assets/img/LogoAzwa.png') }}" alt="Brand" class="w-[128px] h-auto" />
             </div>
         </div>
         <div class="">
-            <ul class="flex">
+            <ul class="flex space-x-8">
                 <li class="">
-                    <a class="text-lg mx-5 text-black font-normal transition duration-300 ease-in-out hover:font-bold hover:underline hover:text-gray-700" href="#">About</a>
+                    <a class="text-lg text-black font-normal transition duration-300 ease-in-out hover:font-bold hover:underline hover:underline-offset-[8px] hover:text-gray-700" href="#">About</a>
                 </li>
                 <li class="">
-                    <a class="text-lg mx-5 text-black font-normal transition duration-300 ease-in-out hover:font-bold hover:underline hover:text-gray-700" href="#">Projects</a>
+                    <a class="text-lg text-black font-normal transition duration-300 ease-in-out hover:font-bold hover:underline hover:underline-offset-[8px] hover:text-gray-700" href="#">Projects</a>
                 </li>
                 <li class="">
-                    <a class="text-lg mx-5 text-black font-normal transition duration-300 ease-in-out hover:font-bold hover:underline hover:text-gray-700" href="#">Services</a>
+                    <a class="text-lg text-black font-normal transition duration-300 ease-in-out hover:font-bold hover:underline hover:underline-offset-[8px] hover:text-gray-700" href="#">Services</a>
                 </li>
                 <li class="">
-                    <a class="text-lg mx-5 text-black font-normal transition duration-300 ease-in-out hover:font-bold hover:underline hover:text-gray-700" href="#">Insights</a>
+                    <a class="text-lg text-black font-normal transition duration-300 ease-in-out hover:font-bold hover:underline hover:underline-offset-[8px] hover:text-gray-700" href="#">Insights</a>
                 </li>
                 <li class="">
-                    <a class="text-lg mx-5 text-black font-normal transition duration-300 ease-in-out hover:font-bold hover:underline hover:text-gray-700" href="#">Career</a>
+                    <a class="text-lg text-black font-normal transition duration-300 ease-in-out hover:font-bold hover:underline hover:underline-offset-[8px] hover:text-gray-700" href="#">Career</a>
                 </li>
                 <li class="">
-                    <a class="text-lg mx-5 text-black font-normal transition duration-300 ease-in-out hover:font-bold hover:underline hover:text-gray-700" href="#">Contact</a>
+                    <a class="text-lg text-black font-normal transition duration-300 ease-in-out hover:font-bold hover:underline hover:underline-offset-[8px] hover:text-gray-700" href="#">Contact</a>
                 </li>
             </ul>
         </div>
         <div class="">
-            <svg xmlns="http://www.w3.org/2000/svg" width="40px" height="40px" viewBox="0 0 24 24"><path fill="black" d="M3 18v-2h18v2zm0-5v-2h18v2zm0-5V6h18v2z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="50px" height="50px" viewBox="0 0 24 24"><path fill="none" stroke="black" stroke-linecap="round" stroke-width="1.5" d="M4 7h3m13 0h-9m9 10h-3M4 17h9m-9-5h16"/></svg>
         </div>
     </div>
 </nav>
