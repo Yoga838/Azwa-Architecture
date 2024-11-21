59fa1b05f9cafbc98ea1de05889cbc5f2e3fe9eb
shrmanto <shrmantoch@gmail.com>
2024-11-21 20:17:56 +0700

    feat: components & home

---
+++ resources/views/components/navigation/nav.blade.php
@@ -0,0 +1,34 @@
+<nav class="fixed top-0 z-50">
+    <div class="flex items-center bg-white py-6 px-6">
+        <div class="logo">
+            <div class="">
+                <Image src="{{ asset('assets/img/LogoAzwa.png') }}" alt="Brand" class="" />
+            </div>
+        </div>
+        <div class="">
+            <ul class="flex">
+                <li class="">
+                    <a class="text-lg mx-5 text-black font-normal transition duration-300 ease-in-out hover:font-bold hover:underline hover:text-gray-700" href="#">About</a>
+                </li>
+                <li class="">
+                    <a class="text-lg mx-5 text-black font-normal transition duration-300 ease-in-out hover:font-bold hover:underline hover:text-gray-700" href="#">Projects</a>
+                </li>
+                <li class="">
+                    <a class="text-lg mx-5 text-black font-normal transition duration-300 ease-in-out hover:font-bold hover:underline hover:text-gray-700" href="#">Services</a>
+                </li>
+                <li class="">
+                    <a class="text-lg mx-5 text-black font-normal transition duration-300 ease-in-out hover:font-bold hover:underline hover:text-gray-700" href="#">Insights</a>
+                </li>
+                <li class="">
+                    <a class="text-lg mx-5 text-black font-normal transition duration-300 ease-in-out hover:font-bold hover:underline hover:text-gray-700" href="#">Career</a>
+                </li>
+                <li class="">
+                    <a class="text-lg mx-5 text-black font-normal transition duration-300 ease-in-out hover:font-bold hover:underline hover:text-gray-700" href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <div class="">
+            <svg xmlns="http://www.w3.org/2000/svg" width="40px" height="40px" viewBox="0 0 24 24"><path fill="black" d="M3 18v-2h18v2zm0-5v-2h18v2zm0-5V6h18v2z"/></svg>
+        </div>
+    </div>
+</nav>
