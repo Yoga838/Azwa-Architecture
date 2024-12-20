2f4099e7735aafc298bd24dabde3c54e959aa207
Ikbar Saif <ikbarsaif@gmail.com>
2024-12-20 21:58:44 +0700

    style: update karir page

--- resources/views/pages/career.blade.php
+++ resources/views/pages/career.blade.php
@@ -2,29 +2,32 @@
 
 @section("content")
 <div class="container mx-auto">
-    <div class="flex flex-col lg:flex-row bg-#e1dfd4 shadow-lg overflow-hidden">
+    <div class="flex flex-col lg:flex-row bg-#e1dfd4 overflow-hidden">
       <!-- Image Section -->
-      <div class="bg-contain lg:w-5/12">
+      <div class="w-auto h-screen lg:w-5/12">
         <img src="{{ asset('assets/img/Karir1.png') }}" alt="Interior Design" class="">
       </div>
       <!-- Content Section -->
-      <div class="w-full lg:w-1/2 p-8 my-auto">
-        <div class="flex items-center space-x-40 mb-10 ">
+      <div class="w-full lg:w-7/12 p-8 my-auto lg:ml-5">
+        <div class="flex items-center space-x-60 mb-10 ">
             <h1 class="text-6xl font-bold text-theme2 mb-4">Karir</h1>
             <div class="h-1 w-100 bg-theme2 rounded-lg"></div>
         </div>
-        <p class="text-2xl text-theme3 pt-7 mb-4">
-          Jelajahi peluang karir yang menarik di <span class="font-bold">Azwa Design Studio!</span>
-          Fulltime / Internship posisi Architect, Interior Designer, Drafter dapat bergabung tim kami.
-        </p>
-        <p class="text-theme3 mb-6">
-          Kirimkan CV dan Portofolio Anda dalam format PDF, tidak lebih dari 5MB ke:
-        </p>
-        <div class="bg-gray-100 p-4 rounded-lg">
-          <p class="text-gray-800 font-semibold">
-            <a href="mailto:azwakaryatama@gmail.com" class="text-blue-500 hover:underline">azwakaryatama@gmail.com</a>
+        <div class="p-2">
+          <p class="text-xl text-theme3 font-medium pt-7 mb-4 ">
+            Jelajahi peluang karir yang menarik di <span class="font-extrabold">Azwa Design Studio! </span> <br>
+            Fulltime / Internship posisi Architect, Interior Designer, Drafter <br> dapat bergabung tim kami.
           </p>
-          <p class="text-gray-600 mt-2">Subject: Posisi_Nama</p>
+          <p class="text-xl text-theme3 font-medium mt-5 mb-20 pt-3">
+            Kirimkan CV dan Portofolio Anda dalam format PDF, <br> lebih dari 5MB ke:
+          </p>
+          <div class="bg-theme2 flex items-center w-fit px-5 rounded-full">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#e1dfd4" d="M4 20q-.825 0-1.412-.587T2 18V6q0-.825.588-1.412T4 4h16q.825 0 1.413.588T22 6v12q0 .825-.587 1.413T20 20zm8-7L4 8v10h16V8zm0-2l8-5H4zM4 8V6v12z"/></svg>
+            <p class="text-xl font-medium pb-1 pl-3">
+              <a href="mailto:azwakaryatama@gmail.com" class="text-theme1 hover:underline">azwakaryatama@gmail.com</a>
+            </p>
+          </div>
+          <p class="text-xl text-theme3 font-medium mt-2">Subject: Posisi_Nama</p>
         </div>
       </div>
     </div>
