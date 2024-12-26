04af1e91f12e866426dfc274109a0004a7e536bf
Yoga838 <212410102031@mail.unej.ac.id>
2024-12-26 23:36:18 +0700

    add api Portofolio

---
+++ app/Http/Controllers/Dashboard/PortofolioController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Models\Portofolio;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class PortofolioController extends Controller
+{
+    public function Store(Request $request){
+        // validate input
+        // dd($request->all());
+
+        $validate = $request->validate([
+            'name' => 'required|string|max:255',
+            'type' => 'required|string|max:100',
+            'date' => 'required|date',
+            'category' => 'required|string|max:100',
+            'luas' => 'required|numeric|min:0',
+            'foto.*' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'kontraktor' => 'required|string|max:255',
+            'deskripsi' => 'nullable|string',
+        ]);
+
+        // save to db
+        $portofolio = new Portofolio();
+        $portofolio->name = $validate['name'];
+        $portofolio->type = $validate['type'];
+        $portofolio->date = $validate['date'];
+        $portofolio->category = $validate['category'];
+        $portofolio->luas = $validate['luas'];
+        $portofolio->kontraktor = $validate['kontraktor'];
+        $portofolio->deskripsi = $validate['deskripsi'];
+        $portofolio->save();
+
+        // proses multiple foto
+
+        if ($request->hasFile('foto')) {
+            foreach ($request->file('foto') as $file) {
+                $path = $file->store('public/portofolio');
+                $portofolio->fotos()->create([
+                    'path' => $path,
+                ]);
+            }
+        }
+
+        // redirect
+        return response()->json(['success' => 'Data Portofolio Berhasil Disimpan!'],201);
+    }
+
+    public function get(Request $request){
+        $portofolio = Portofolio::with('fotos')->get();
+        return response()->json(['data'=>$portofolio],200);
+    }
+
+    public function edit(Request $request, $id){
+        $portofolio = Portofolio::findOrFail($id);
+        dd($request->all());
+        $validate = $request->validate([
+            'name' => 'required|string|max:255',
+            'type' => 'required|string|max:100',
+            'date' => 'required|date',
+            'category' => 'required|string|max:100',
+            'luas' => 'required|numeric|min:0',
+            'foto.*' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'kontraktor' => 'required|string|max:255',
+            'deskripsi' => 'nullable|string',
+        ]);
+
+        $portofolio->update($validate);
+
+        if ($request->hasFile('foto')) {
+            foreach ($request->file('foto') as $file) {
+                $path = $file->store('public/portofolio');
+                $portofolio->fotos()->create([
+                    'path' => $path,
+                ]);
+            }
+        }
+
+        return response()->json(['success' => 'Data Portofolio Berhasil Diupdate!'],201);
+    }
+
+    public function delete($id){
+        $portofolio = Portofolio::findOrFail($id);
+
+        foreach ($portofolio->fotos as $foto) {
+            Storage::delete($foto->path);
+            $foto->delete();
+        }
+
+        $portofolio->delete();
+        return response()->json(['success' => 'Data Portofolio Berhasil Dihapus!'],201);
+    }
+    public function getById($id){
+        // Ambil portofolio berdasarkan ID dan termasuk relasi fotos
+        $portofolio = Portofolio::with('fotos')->findOrFail($id);
+
+        return response()->json(['data' => $portofolio], 200);
+    }
+
+}
---
+++ database/migrations/2024_12_26_143719_create_portofolios_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('portofolio', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',255);
+            $table->string('type',100);
+            $table->date('date');
+            $table->string('category',100);
+            $table->decimal('luas',10,2);
+            $table->string('kontraktor',255);
+            $table->text('deskripsi')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('portofolio');
+    }
+};
