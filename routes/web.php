a6dfbde704e4baaa98e34a570d0ecf48c06d9b24
shrmanto <shrmantoch@gmail.com>
2024-12-26 21:49:10 +0700

    fix: route

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,15 @@ use App\Http\Controllers\Pages\InsightsController;
 use App\Http\Controllers\Pages\ProjectsController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
-Route::get('/', function () {
-    return view('pages.home');
-});
+Route::get('/',[HomeController::class, 'index'])->name('home.index');
+Route::get('/about',[AboutController::class, 'index'])->name('about.index');
+Route::get('/project',[ProjectsController::class, 'index'])->name('project.index');
+Route::get('/service',[ServiceController::class, 'index'])->name('service.index');
+Route::get('/insight',[InsightsController::class, 'index'])->name('insight.index');
+Route::get('/career',[CareerController::class, 'index'])->name('career.index');
+Route::get('/contact',[ContactController::class, 'index'])->name('contact.index');
 
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
-
 
 Route::get('/dashboard', function () {
     return view('admin.home.index');
@@ -28,13 +31,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::get('/home',[HomeController::class, 'index'])->name('home.index');
-Route::get('/about',[AboutController::class, 'index'])->name('about.index');
-Route::get('/project',[ProjectsController::class, 'index'])->name('project.index');
-Route::get('/service',[ServiceController::class, 'index'])->name('service.index');
-Route::get('/insight',[InsightsController::class, 'index'])->name('insight.index');
-Route::get('/career',[CareerController::class, 'index'])->name('career.index');
-Route::get('/contact',[ContactController::class, 'index'])->name('contact.index');
 
 require __DIR__.'/auth.php';
