72e9c656d006be4c3e5c6c3fec43a5e2d730fc1b
RendyRensFirmansyah <rendyfirmansyah.job@gmail.com>
2024-12-29 03:21:56 +0700

    feat: component table paket service desain

---
+++ resources/views/components/tabel-paket/tabel-paket.blade.php
@@ -0,0 +1,157 @@
+<div class="overflow-x-auto p-4">
+    <table class="w-full border-collapse border-0">
+      <thead>
+        <tr class="bg-landing-black-1 text-white">
+          <th class="border-0 border-white text-center p-4 rounded-tl-full rounded-bl-full" rowspan="2">Fitur</th>
+          <th class="border-l-0 border-b border-white text-center p-4" colspan="2">ARSITEKTUR</th>
+          <th class="border-l border-b border-r border-white text-center p-4" colspan="2">INTERIOR</th>
+          <th class="border-l border-b border-white text-center p-4 rounded-tr-full" colspan="2">ARSITEKTUR INTERIOR</th>
+        </tr>
+        <tr class="bg-landing-black-1 text-white">
+          <th class="border-0 text-landing-gold font-extrabold text-center p-4 text-lg">BRONZE<br><span class="font-normal text-landing-gold">35.000 / m²</span></th>
+          <th class="border-r text-landing-gold font-extrabold text-center p-4 text-lg">GOLD<br><span class="font-normal text-landing-gold">50.000 / m²</span></th>
+          <th class="border-0 text-landing-gold font-extrabold text-center p-4 text-lg">DELUXE<br><span class="font-normal text-landing-gold">100.000 / m²</span></th>
+          <th class="border-r text-landing-gold font-extrabold text-center p-4 text-lg">PREMIUM<br><span class="font-normal text-landing-gold">150.000 / m²</span></th>
+          <th class="border-0 text-landing-gold font-extrabold text-center p-4 text-lg">PLATINUM<br><span class="font-normal text-landing-gold">75.000 / m²</span></th>
+          <th class="border-0 text-landing-gold font-extrabold text-center p-4 text-lg rounded-br-[3.5rem]">PLATINUM+<br><span class="font-normal text-landing-gold">125.000 / m²</span></th>
+        </tr>
+      </thead>
+      <tbody class="bg-white">
+        <tr>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-start p-2 w-[15%]">DENAH</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+        </tr>
+        <tr>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-start p-2 w-[15%]">3D RENDER EKSTERIOR</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+        </tr>
+        <tr>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-start p-2 w-[15%]">LAYOUT PRESENTATION + 3D MODEL</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+        </tr>
+        <tr>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-start p-2 w-[15%]">GAMBAR KERJA ARSITEKTUR</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+        </tr>
+        <tr>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-start p-2 w-[15%]">GAMBAR KERJA STRUKTUR</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+        </tr>
+        <tr>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-start p-2 w-[15%]">GAMBAR KERJA MEP</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+        </tr>
+        <tr>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-start p-2 w-[15%]">RAB</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+        </tr>
+        <tr>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-start p-2 w-[15%]">VIDEO 3D</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+        </tr>
+        <tr>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-start p-2 w-[15%]">DOKUMEN HASIL</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+        </tr>
+        <tr>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-start p-2 w-[15%]">3D RENDER INTERIOR</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">ALL VIEW</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">ALL VIEW</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">2 VIEW / RUANG</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">ALL VIEW</td>
+        </tr>
+        <tr>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-start p-2 w-[15%]">GAMBAR KERJA & SPESIFIKASI MATERIAL</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+        </tr>
+        <tr>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-start p-2 w-[15%]">DETAIL UKURAN FURNITUR (2D & 3D)</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+        </tr>
+        <tr>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-start p-2 w-[15%]">TABEL MATERIAL</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+        </tr>
+        <tr>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-start p-2 w-[15%]">DETAIL POTONGAN RUANGAN</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+        </tr>
+        <tr>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-start p-2 w-[15%]">3D MOODBOARD</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-table text-center p-2">❌</td>
+          <td class="border-b-[1px] border-landing-brown-3 bg-landing-body text-center p-2">✔️</td>
+        </tr>
+      </tbody>
+    </table>
+</div>
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,40 +1,49 @@
 @extends("landing")
 
 @section("content")
-    <div class="w-full h-full flex justify-center items-center relative">
-        <div class="absolute z-[-1] w-full h-full overflow-hidden">
+    <div class="w-full h-screen flex justify-center items-center relative">
+        <div class="absolute z-[-1] overflow-hidden">
             <img src="{{asset('assets/img/about-us/bg-aboutUs.png')}}" alt="" class="w-screen object-cover h-screen">
         </div>
-        <div class="py-32">
-            {{-- <h1 class="font-bold text-[100px] text-landing-black-2 text-center">About Us</h1> --}}
+        <div class="mt-20 md:mt-0">
             <div class="flex justify-center">
-                <img src="{{asset('assets/img/LogoAzwa.png')}}" alt="" class="w-[10%]">
+                <img src="{{asset('assets/img/LogoAzwa.png')}}" alt="" class="w-[25%] md:w-[15%]">
             </div>
+            {{-- <h1 class="font-bold text-[100px] text-landing-black-2 text-center">About Us</h1> --}}
             <div class="lg:mx-44 md:mx-20 mx-10">
-                <p class="mt-12 text-center text-white lg:text-3xl md:text-xl text-lg"><strong>Azwa Design Studio</strong> merupakan penyedia layanan desain arsitektur yang berkomitmen untuk menciptakan bangunan yang menginspirasi dan memenuhi kebutuhan Anda dengan menawarkan solusi desain yang inovatif, fungsional, dan estetis. 
+                <p class="mt-12 text-center text-white lg:text-2xl md:text-xl text-lg"><strong>Azwa Design Studio</strong> merupakan penyedia layanan desain arsitektur yang berkomitmen untuk menciptakan bangunan yang menginspirasi dan memenuhi kebutuhan Anda dengan menawarkan solusi desain yang inovatif, fungsional, dan estetis. 
                 </p>
-                <p class="mt-8 text-center text-white lg:text-3xl md:text-xl text-lg">Kami percaya bahwa setiap proyek adalah cerminan dari identitas dan gaya hidup Anda, dan kami berusaha menghadirkan desain arsitektur yang berkualitas tinggi dan memuaskan Anda. </p>
+                <p class="mt-8 text-center text-white lg:text-2xl md:text-xl text-lg">Kami percaya bahwa setiap proyek adalah cerminan dari identitas dan gaya hidup Anda, dan kami berusaha menghadirkan desain arsitektur yang berkualitas tinggi dan memuaskan Anda. </p>
             </div>
         </div>
     </div>
-    <div class="py-28">
+    <div class="py-10">
         <div class="flex flex-wrap" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1000">
             <div class="w-full md:w-1/3 text-center">
-                <span class="num text-8xl text-theme2 font-bold" data-val="5">0</span><span class="text-6xl text-theme2 font-bold">+</span>
+                <div class="flex justify-center items-center">
+                    <span class="num text-8xl text-theme2 font-extrabold" data-val="5">0</span>
+                    <span class="text-6xl text-theme2 font-extrabold">+</span>
+                </div>
                 <h1 class="text-theme2 text-2xl font-bold mt-8">TAHUN</br>BEROPERASI</h1>
             </div>
             <div class="w-full md:w-1/3 text-center">
-                <span class="num text-8xl text-theme2 font-bold" data-val="700">0</span><span class="text-6xl text-theme2 font-bold">+</span>
+                <div class="flex justify-center items-center">
+                    <span class="num text-8xl text-theme2 font-extrabold" data-val="700">0</span>
+                    <span class="text-6xl text-theme2 font-extrabold">+</span>
+                </div>
                 <h1 class="text-theme2 text-2xl font-bold mt-8">PROYEK</br>TERSELESAIKAN</h1>
             </div>
             <div class="w-full md:w-1/3 text-center">
-                <span class="num text-8xl text-theme2 font-bold" data-val="100">0</span><span class="text-6xl text-theme2 font-bold">+</span>
+                <div class="flex justify-center items-center">
+                    <span class="num text-8xl text-theme2 font-extrabold" data-val="100">0</span>
+                    <span class="text-6xl text-theme2 font-extrabold">+</span>
+                </div>
                 <h1 class="text-theme2 text-2xl font-bold mt-8">PROYEK</br>TERBANGUN</h1>
             </div>
         </div>
     </div>
     {{-- Gmaps Marker --}}
-    <div class="px-0" data-aos="zoom-in">
+    <div class="mx-5 mt-5 border-8 border-dashed border-theme2">
         <gmp-map
             center="-2.548926,118.0148634"
             zoom="5"
@@ -45,17 +54,19 @@
         </gmp-map>
     </div>
     {{-- Visi --}}
-    <div class="py-22 px-32">
-        <div class="flex flex-wrap">
-            <div class="w-full md:w-1/4">
-                <h1 class="text-5xl font-bold text-theme2">Visi</h1>
-            </div>
-            <div class="w-full md:w-9/12 flex justify-center items-center">
-                <div class="h-1 w-full bg-theme2 rounded-lg"></div>
+    <div class="py-20">
+        <div class="px-32">
+            <div class="flex flex-wrap">
+                <div class="w-full md:w-1/4">
+                    <h1 class="text-5xl font-bold text-theme2">Visi</h1>
+                </div>
+                <div class="w-full md:w-9/12 flex justify-center items-center">
+                    <div class="h-1 w-full bg-theme2 rounded-lg"></div>
+                </div>
             </div>
         </div>
-        <div class="title-visi text-center py-8">
-            <p class="font-light text-theme2 text-base mx-8">Terdepan dalam menghadirkan desain arsitektur yang elegan, fungsional, dan ramah bagi semua kalangan, memberikan pengalaman estetika dan kenyamanan yang melampaui ekspektasi.</p>
+        <div class="title-visi text-center py-8 px-10 lg:px-40">
+            <p class="font-light text-theme2 text-xl">Terdepan dalam menghadirkan desain arsitektur yang elegan, fungsional, dan ramah bagi semua kalangan, memberikan pengalaman estetika dan kenyamanan yang melampaui ekspektasi.</p>
         </div>
     </div>
     {{-- Misi --}}
@@ -68,16 +79,16 @@
                 <div class="h-1 w-full bg-theme2 rounded-lg"></div>
             </div>
         </div>
-        <div class="title-visi py-10 mb-10">
-            <ul class="space-y-3">
-                <li class="list-disc mx-20 text-theme2"><p class="font-light text-theme2 text-base">Menciptakan karya arsitektur yang menggabungkan estetika elegan dengan fungsi optimal, memastikan setiap desain memiliki nilai estetika yang tinggi sekaligus memenuhi kebutuhan penghuni.
-                </p></li>
-                <li class="list-disc mx-20 text-theme2"><p class="font-light text-theme2 text-base">Menyediakan solusi desain yang terjangkau tanpa mengurangi kualitas, sehingga setiap individu, terlepas dari latar belakang ekonomi, dapat merasakan keindahan dan kenyamanan arsitektur berkualitas.
-                </p></li>
-                <li class="list-disc mx-20 text-theme2"><p class="font-light text-theme2 text-base">Memanfaatkan teknologi terbaru dalam proses desain dan konstruksi untuk memberikan efisiensi, presisi, dan pengalaman klien yang lebih baik.
-                </p></li>
-            </ul>
-        </div>
+    </div>
+    <div class="title-visi py-10 px-15 lg:px-50 mb-10">
+        <ul class="space-y-3">
+            <li class="list-disc text-theme2"><p class="font-light text-theme2 text-xl text-justify lg:text-start">Menciptakan karya arsitektur yang menggabungkan estetika elegan dengan fungsi optimal, memastikan setiap desain memiliki nilai estetika yang tinggi sekaligus memenuhi kebutuhan penghuni.
+            </p></li>
+            <li class="list-disc text-theme2"><p class="font-light text-theme2 text-xl text-justify lg:text-start">Menyediakan solusi desain yang terjangkau tanpa mengurangi kualitas, sehingga setiap individu, terlepas dari latar belakang ekonomi, dapat merasakan keindahan dan kenyamanan arsitektur berkualitas.
+            </p></li>
+            <li class="list-disc text-theme2"><p class="font-light text-theme2 text-xl text-justify lg:text-start">Memanfaatkan teknologi terbaru dalam proses desain dan konstruksi untuk memberikan efisiensi, presisi, dan pengalaman klien yang lebih baik.
+            </p></li>
+        </ul>
     </div>
 
     <script src="{{asset('resources/script.js')}}"></script>
