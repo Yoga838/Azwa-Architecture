cee7004453eb62139f7c93231b9e6f6e6d6a42b7
Ahmad Rizky Febianto <ahmadrizky13jkl@gmail.com>
2025-01-23 00:49:22 +0700

    update : view

--- resources/views/pages/service-kontraktor.blade.php
+++ resources/views/pages/service-kontraktor.blade.php
@@ -297,17 +297,17 @@
     </div>
     <div class="swiper-pagination"></div>
     </div>
-    <div class="flex justify-center invisible mt-2 2xl:visible xl:visible lg:invisible md:invisible sm:invisible">
-        <button class="px-8 py-2 transition duration-300 ease-in-out transform border rounded-full bg-landing-brown-1 hover:bg-landing-brown-3 hover:scale-105 2xl:px-9 xl:px-8 2xl:py-3 xl:py-2">
-            <div class="flex items-center gap-2 text-xs 2xl:text-xl xl:text-base lg:text-base md:text-xs">
-                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 50 50">
-                    <path d="M 16 3 C 8.8324839 3 3 8.8324839 3 16 L 3 34 C 3 41.167516 8.8324839 47 16 47 L 34 47 C 41.167516 47 47 41.167516 47 34 L 47 16 C 47 8.8324839 41.167516 3 34 3 L 16 3 z M 16 5 L 34 5 C 40.086484 5 45 9.9135161 45 16 L 45 34 C 45 40.086484 40.086484 45 34 45 L 16 45 C 9.9135161 45 5 40.086484 5 34 L 5 16 C 5 9.9135161 9.9135161 5 16 5 z M 37 11 A 2 2 0 0 0 35 13 A 2 2 0 0 0 37 15 A 2 2 0 0 0 39 13 A 2 2 0 0 0 37 11 z M 25 14 C 18.936712 14 14 18.936712 14 25 C 14 31.063288 18.936712 36 25 36 C 31.063288 36 36 31.063288 36 25 C 36 18.936712 31.063288 14 25 14 z M 25 16 C 29.982407 16 34 20.017593 34 25 C 34 29.982407 29.982407 34 25 34 C 20.017593 34 16 29.982407 16 25 C 16 20.017593 20.017593 16 25 16 z"></path>
-                </svg>
-                <h1>Follow us!</h1>
-            </div>
-        </button>
+    <div class="relative flex justify-center invisible mt-2 z-9 2xl:visible xl:visible lg:invisible md:invisible sm:invisible">
+            <a href="https://www.instagram.com/azwadesignstudio?igsh=MTVmM3B6cXlzaDhybg==" class="px-8 py-2 transition duration-300 ease-in-out transform border rounded-full bg-landing-brown-1 hover:bg-landing-brown-3 hover:scale-105 2xl:px-9 xl:px-8 2xl:py-3 xl:py-2">
+                <div class="flex items-center gap-2 text-xs 2xl:text-xl xl:text-base lg:text-base md:text-xs">
+                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 50 50">
+                        <path d="M 16 3 C 8.8324839 3 3 8.8324839 3 16 L 3 34 C 3 41.167516 8.8324839 47 16 47 L 34 47 C 41.167516 47 47 41.167516 47 34 L 47 16 C 47 8.8324839 41.167516 3 34 3 L 16 3 z M 16 5 L 34 5 C 40.086484 5 45 9.9135161 45 16 L 45 34 C 45 40.086484 40.086484 45 34 45 L 16 45 C 9.9135161 45 5 40.086484 5 34 L 5 16 C 5 9.9135161 9.9135161 5 16 5 z M 37 11 A 2 2 0 0 0 35 13 A 2 2 0 0 0 37 15 A 2 2 0 0 0 39 13 A 2 2 0 0 0 37 11 z M 25 14 C 18.936712 14 14 18.936712 14 25 C 14 31.063288 18.936712 36 25 36 C 31.063288 36 36 31.063288 36 25 C 36 18.936712 31.063288 14 25 14 z M 25 16 C 29.982407 16 34 20.017593 34 25 C 34 29.982407 29.982407 34 25 34 C 20.017593 34 16 29.982407 16 25 C 16 20.017593 20.017593 16 25 16 z"></path>
+                    </svg>
+                    <h1>Follow us!</h1>
+                </div>
+            </a>
     </div>
-    <div class="absolute top-[50%] left-[50.5%] transform -translate-x-1/2 -translate-y-1/2 lg:visible md:invisible sm:invisible invisible">
+    <div class="absolute top-[50%] left-[50.5%] transform -translate-x-1/2 -translate-y-1/2 lg:visible md:invisible sm:invisible invisible z-0">
         <img src="{{asset('assets/img/service/kontraktor/bg-framehp.png')}}" alt="Frame" class="w-[100%] h-auto">
     </div>
     </div>
